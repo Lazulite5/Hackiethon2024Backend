b8c132027079bd7bf0965cfdf17ac3ec8d4cc72e
Lazulite5 <s969845838@gmail.com>
2024-03-23 13:13:27 +1100

    testing2

--- Submissions/opponent1.py
+++ Submissions/opponent1.py
@@ -10,8 +10,8 @@ from gameSettings import HP, LEFTBORDER, RIGHTBORDER, LEFTSTART, RIGHTSTART, PAR
 # SECONDARY CAN BE : Hadoken, Grenade, Boomerang, Bear Trap
 
 # TODO FOR PARTICIPANT: Set primary and secondary skill here
-PRIMARY_SKILL = Meditate
-SECONDARY_SKILL = Grenade
+PRIMARY_SKILL = TeleportSkill
+SECONDARY_SKILL = Hadoken
 
 #constants, for easier move return
 #movements
@@ -35,40 +35,21 @@ NOMOVE = "NoMove"
 moves = SECONDARY,
 moves_iter = iter(moves)
 
-
 # TODO FOR PARTICIPANT: WRITE YOUR WINNING BOT
 class Script:
     def __init__(self):
         self.primary = PRIMARY_SKILL
         self.secondary = SECONDARY_SKILL
-        #self.current_tick = 0
+        
     # DO NOT TOUCH
     def init_player_skills(self):
         return self.primary, self.secondary
     
     # MAIN FUNCTION that returns a single move to the game manager
     def get_move(self, player, enemy, player_projectiles, enemy_projectiles):
-        # current_tick += 1
-        # if current_tick == 2:
-        #     return JUMP
-
         distance = abs(get_pos(player)[0] - get_pos(enemy)[0])
 
-        if get_primary_cooldown(player) == 0:
-            if get_hp(player) < 80:
-                return PRIMARY
-        if distance <= 5 and get_secondary_cooldown(player) == 0:
-            return SECONDARY
-
-        if distance == 1 and get_stun_duration(enemy) != 0: 
-            #if enemy stun, light attack
+        if distance == 1:
             return LIGHT
-        elif distance == 1:
-            return LIGHT
-        elif distance == 2:
-            return LIGHT
-        elif distance > 2:
-            return FORWARD
-        return LIGHT
-
         
+        return FORWARD
